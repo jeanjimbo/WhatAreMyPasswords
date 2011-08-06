ce920d8f225d5b74617c0b8b130643e43b350d43
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-06 22:16:58 +0300

    layouts inflated

--- src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
+++ src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
@@ -1,5 +1,16 @@
 package ebeletskiy.gmail.com.passwords;
 
-public class ItemsDescription {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class ItemsDescription extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.items_description, container, false);
+	}
 }
--- src/ebeletskiy/gmail/com/passwords/ItemsList.java
+++ src/ebeletskiy/gmail/com/passwords/ItemsList.java
@@ -1,5 +1,16 @@
 package ebeletskiy.gmail.com.passwords;
 
-public class ItemsList {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class ItemsList extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.items_list, container, false);
+	}
 }
