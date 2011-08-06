d42cfde498dc1fd795c137478e46dc461319f982
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-06 22:17:59 +0300

    Background to ActionBar added

--- src/ebeletskiy/gmail/com/passwords/WhatAreMyPasswordsActivity.java
+++ src/ebeletskiy/gmail/com/passwords/WhatAreMyPasswordsActivity.java
@@ -1,6 +1,9 @@
 package ebeletskiy.gmail.com.passwords;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
 public class WhatAreMyPasswordsActivity extends Activity {
@@ -9,5 +12,12 @@ public class WhatAreMyPasswordsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ActionBar actionBar = getActionBar();
+		BitmapDrawable background = new BitmapDrawable(
+				BitmapFactory.decodeResource(getResources(),
+						R.drawable.actionbar_back));
+		background.setTileModeX(android.graphics.Shader.TileMode.REPEAT);
+		actionBar.setBackgroundDrawable(background);
     }
 }
