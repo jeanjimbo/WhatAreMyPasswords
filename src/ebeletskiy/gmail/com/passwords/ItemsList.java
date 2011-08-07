da789eb61370238388d553af04e718cf7fa657e8
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-07 20:17:30 +0300

    Custom Cursor Adapter along with ViewHolder implemented

--- src/ebeletskiy/gmail/com/passwords/ItemsList.java
+++ src/ebeletskiy/gmail/com/passwords/ItemsList.java
@@ -1,17 +1,62 @@
 package ebeletskiy.gmail.com.passwords;
 
+import android.app.Activity;
 import android.app.ListFragment;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+import ebeletskiy.gmail.com.passwords.utils.DBHelper;
 
 public class ItemsList extends ListFragment {
 	private static final String TAG = "ItemsList";
+	
+	private DBHelper dbHelper;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.items_list, container, false);
+	}
+	
+	private class MAdapter extends CursorAdapter {
+		
+		public MAdapter(Context context, Cursor c, boolean autoRequery) {
+			super(context, c, autoRequery);
+		}
+
+		@Override
+		public void bindView(View row, Context ctxt, Cursor c) {
+			ViewHolder holder=(ViewHolder)row.getTag();
+			
+			holder.populateFrom(c, dbHelper);
+		}
+		
+		@Override
+		public View newView(Context ctxt, Cursor c, ViewGroup parent) {
+			LayoutInflater inflater=((Activity)ctxt).getLayoutInflater();
+			View row=inflater.inflate(R.layout.row, parent, false);
+			ViewHolder holder=new ViewHolder(row);
+			
+			row.setTag(holder);
+			
+			return(row);
+		}
+	}
+
+	static class ViewHolder {
+		private TextView name;
+		
+		ViewHolder(View row) {
+			name = (TextView)row.findViewById(R.id.title);
+		}
+		
+		void populateFrom(Cursor c, DBHelper helper) {
+			name.setText(helper.getTitle(c));
+		}
 	}
 }
