bb3ccfd9479a3941a69183df367d7e6e111be388
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-07 19:02:49 +0300

    extending from ListFragment

--- src/ebeletskiy/gmail/com/passwords/ItemsList.java
+++ src/ebeletskiy/gmail/com/passwords/ItemsList.java
@@ -1,12 +1,12 @@
 package ebeletskiy.gmail.com.passwords;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ItemsList extends Fragment {
+public class ItemsList extends ListFragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
