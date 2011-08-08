902fefdc6dc6e859162ef107f512f8be12af8bd1
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-08 23:36:41 +0300

    Data model defined

---
+++ src/ebeletskiy/gmail/com/passwords/models/Ticket.java
@@ -0,0 +1,42 @@
+package ebeletskiy.gmail.com.passwords.models;
+
+public class Ticket {
+	private int id;
+	private String title;
+	private String login;
+	private String password;
+	private String notes;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+}
