91da540723e4da3246f65b53b88c1db8a0f310a5
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-09 02:18:45 +0300

    column indexes corrected

--- src/ebeletskiy/gmail/com/passwords/utils/DataConverter.java
+++ src/ebeletskiy/gmail/com/passwords/utils/DataConverter.java
@@ -4,17 +4,24 @@ import android.database.Cursor;
 import ebeletskiy.gmail.com.passwords.models.Ticket;
 
 public class DataConverter {
-
+	private static final String TAG = "DataConverter";
+	
 	public static Ticket convertToTicket(Cursor result) {
 		Ticket ticket = new Ticket();
 		
-		while (!result.moveToNext()) {
+//		String str = " ";
+//		String [] stringarray = result.getColumnNames();
+//		  for (int i = 0; i < stringarray.length; i++) {
+//		  str = str + stringarray[i];
+//		  }
+//		Log.i(TAG, str);
+		
+		result.moveToFirst();
 			ticket.setId(result.getInt(0));
-			ticket.setTitle(result.getString(1));
-			ticket.setLogin(result.getString(2));
-			ticket.setPassword(result.getString(3));
-			ticket.setNotes(result.getString(4));
-		}
+			ticket.setTitle(result.getString(1)); 
+			ticket.setLogin(result.getString(2)); 
+			ticket.setPassword(result.getString(3)); 
+			ticket.setNotes(result.getString(4)); 
 	
 		result.close();
 		return ticket;
