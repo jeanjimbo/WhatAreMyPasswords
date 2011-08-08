fc0bae52c74c05aa3ccab3792543c0afd073a5d2
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-09 02:15:34 +0300

    Method changed

--- src/ebeletskiy/gmail/com/passwords/ListItemClickListener.java
+++ src/ebeletskiy/gmail/com/passwords/ListItemClickListener.java
@@ -1,5 +1,7 @@
 package ebeletskiy.gmail.com.passwords;
 
+import ebeletskiy.gmail.com.passwords.models.Ticket;
+
 public interface ListItemClickListener {
-	void itemClicked();
+	void showTicket(Ticket ticket);
 }
