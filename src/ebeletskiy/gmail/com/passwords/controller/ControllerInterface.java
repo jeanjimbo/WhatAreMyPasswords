ba4290155f3491972880ba5e2ae16cfa9d3e6c2d
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-10 21:56:32 +0300

    Methods declared

--- src/ebeletskiy/gmail/com/passwords/controller/ControllerInterface.java
+++ src/ebeletskiy/gmail/com/passwords/controller/ControllerInterface.java
@@ -1,5 +1,32 @@
 package ebeletskiy.gmail.com.passwords.controller;
 
-public interface ControllerInterface {
+import android.database.Cursor;
+import ebeletskiy.gmail.com.passwords.models.DataModel;
 
+public interface ControllerInterface {
+	/**
+	 * Retrieve all columns from user table
+	 * @return Cursor
+	 */
+	Cursor getAllData();
+	
+	
+	/**
+	 * Inserts provided DataModel into database
+	 * @param DataModel data
+	 */
+	void insertItem(DataModel data);
+	
+	
+	/**
+	 * Deletes provided DataModel item from database
+	 * @param DataModel
+	 */
+	void deleteItem(DataModel data);
+	
+	
+	/** 
+	 * Delets all data from user database
+	 */
+	void deleteAllItems();
 }
