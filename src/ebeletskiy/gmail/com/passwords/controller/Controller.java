60d3e96be2c3af0713294d79b6a701ae944ff620
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-10 21:56:13 +0300

    Interface's methods declared

--- src/ebeletskiy/gmail/com/passwords/controller/Controller.java
+++ src/ebeletskiy/gmail/com/passwords/controller/Controller.java
@@ -1,5 +1,32 @@
 package ebeletskiy.gmail.com.passwords.controller;
 
+import android.database.Cursor;
+import ebeletskiy.gmail.com.passwords.models.DataModel;
+
 public class Controller implements ControllerInterface{
+
+	@Override
+	public Cursor getAllData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertItem(DataModel data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteItem(DataModel data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllItems() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
