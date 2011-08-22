f6f452b7929cd83150c0998c19220f966ca42691
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-22 18:15:27 +0300

    Empty frag

---
+++ src/ebeletskiy/gmail/com/passwords/EmptyRightFrag.java
@@ -0,0 +1,19 @@
+package ebeletskiy.gmail.com.passwords;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EmptyRightFrag extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.empty_right_frag, container, false);
+	}
+	
+	public static Fragment newInstance() {
+		return new EmptyRightFrag();
+	}
+}
