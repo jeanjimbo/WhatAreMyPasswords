14a0349fbd150cfe0621e3b5e2eb05ef61cbf603
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-22 14:56:01 +0300

    Menu defined

--- src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
+++ src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
@@ -3,6 +3,9 @@ package ebeletskiy.gmail.com.passwords;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -17,5 +20,20 @@ public class ItemsDescription extends Fragment {
 
 	public static Fragment newInstance() {
 		return new ItemsDescription();
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		if (savedInstanceState == null) {
+			setHasOptionsMenu(true);
+		}
+	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		 menu.add("Show password").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+         menu.add("Delete").setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
 	}
 }
