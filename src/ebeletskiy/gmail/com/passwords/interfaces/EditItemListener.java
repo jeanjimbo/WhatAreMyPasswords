477bc328e0f7d01f62a928b8e74a23e2cc5a6a3c
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-26 21:06:58 +0300

    Interface for edit saving created

---
+++ src/ebeletskiy/gmail/com/passwords/interfaces/EditItemListener.java
@@ -0,0 +1,7 @@
+package ebeletskiy.gmail.com.passwords.interfaces;
+
+import ebeletskiy.gmail.com.passwords.models.Ticket;
+
+public interface EditItemListener {
+	void loadEditItem(Ticket ticket);
+}
