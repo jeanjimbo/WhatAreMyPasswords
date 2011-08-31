448731c3b33bb7d2c5305d52df677097fd83abdd
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-08-31 21:30:59 +0300

    Styles for portrait mode changed to fit screen size. Change state for ItemsDesrption handled.

--- src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
+++ src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
@@ -13,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import ebeletskiy.gmail.com.passwords.interfaces.DeleteItemListener;
@@ -71,19 +72,15 @@ public class ItemsDescription extends Fragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		if (savedInstanceState == null) {
-			title = (TextView)getView().findViewById(R.id.tv_title);
-			title.setText(ticket.getTitle());
-			
-			login = (TextView)getView().findViewById(R.id.tv_login_data);
-			login.setText(ticket.getLogin());
-			
-			password = (TextView)getView().findViewById(R.id.tv_password_data);
-			password.setText(ticket.getPassword());
-			
-			notes = (TextView)getView().findViewById(R.id.tv_notes_data);
-			notes.setText(ticket.getNotes());
-		}		
+		
+		initUI();
+		
+		if (savedInstanceState != null) {
+			title.setText( (String)savedInstanceState.get("title") );
+			login.setText( (String)savedInstanceState.get("login") );
+			password.setText( (String)savedInstanceState.get("password") );
+			notes.setText( (String)savedInstanceState.get("notes") );
+		}
 	}
 	
 	
@@ -138,6 +135,28 @@ public class ItemsDescription extends Fragment {
 				Toast.LENGTH_SHORT);
 		t.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER, 0, 0);
 		t.show();
+	}
+	
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString("title", title.getText().toString());
+		outState.putString("login", login.getText().toString());
+		outState.putString("password", password.getText().toString());
+		outState.putString("notes", notes.getText().toString());
+	}
+	
+	private void initUI() {
+		title = (TextView)getView().findViewById(R.id.tv_title);
+		login = (TextView)getView().findViewById(R.id.tv_login_data);
+		password = (TextView)getView().findViewById(R.id.tv_password_data);
+		notes = (TextView)getView().findViewById(R.id.tv_notes_data);
+
+		if (ticket != null) {
+			title.setText(ticket.getTitle());
+			login.setText(ticket.getLogin());
+			password.setText(ticket.getPassword());
+			notes.setText(ticket.getNotes());
+		}
 	}
 	
 }
