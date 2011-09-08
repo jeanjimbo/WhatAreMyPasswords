e63fa23d54f10127609073b3b1100a2f730785d1
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-09-08 22:58:45 +0300

    Show eye icon only for items where password defined. Set back password to invisible on even click.

--- src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
+++ src/ebeletskiy/gmail/com/passwords/ItemsDescription.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -24,42 +25,37 @@ import ebeletskiy.gmail.com.passwords.utils.DBHelper;
 public class ItemsDescription extends Fragment {
 	private static final String TAG = "ItemsDescription";
 	private boolean menuWasCreated = false;
-	
-	Ticket ticket; 
-	TextView title, login, password, notes;
-	DBHelper dbHelper;
-	DeleteItemListener deleteListener;
-	EditItemListener editItemListener;
-	
+	private boolean passwordShown = true;
+
+	private Ticket ticket;
+	private TextView title, login, password, notes;
+	private DBHelper dbHelper;
+	private DeleteItemListener deleteListener;
+	private EditItemListener editItemListener;
+
 	public ItemsDescription() {
-		
+
 	}
-	
+
 	public ItemsDescription(Ticket ticket) {
-		if (ticket == null) {		
+		if (ticket == null) {
 			throw new IllegalArgumentException();
 		}
-		
-		this.ticket = ticket;	
+
+		this.ticket = ticket;
 	}
-	
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
-		deleteListener = (DeleteItemListener)activity;
-		editItemListener = (EditItemListener)activity;
+		deleteListener = (DeleteItemListener) activity;
+		editItemListener = (EditItemListener) activity;
 	}
-	
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		dbHelper = new DBHelper(getActivity());
-		
-		if (!menuWasCreated) {
-			setHasOptionsMenu(true);
-			menuWasCreated = true;
-		}
 	}
 
 	@Override
@@ -67,75 +63,96 @@ public class ItemsDescription extends Fragment {
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.items_description, container, false);
 	}
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
 		initUI();
-		
+
+		if (!menuWasCreated) {
+			setHasOptionsMenu(true);
+			menuWasCreated = true;
+		}
+
 		if (savedInstanceState != null) {
-			title.setText( (String)savedInstanceState.get("title") );
-			login.setText( (String)savedInstanceState.get("login") );
-			password.setText( (String)savedInstanceState.get("password") );
-			notes.setText( (String)savedInstanceState.get("notes") );
+			title.setText((String) savedInstanceState.get("title"));
+			login.setText((String) savedInstanceState.get("login"));
+			password.setText((String) savedInstanceState.get("password"));
+			notes.setText((String) savedInstanceState.get("notes"));
 		}
 	}
-	
-	
+
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-	    inflater.inflate(R.menu.menu_items_description, menu);
+		inflater.inflate(R.menu.menu_items_description, menu);
 	}
-	
-	
+
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+		MenuItem showPassItem = menu.getItem(3);
+
+		if (password != null) {
+			if (password.getText().toString().equals("")) {
+				showPassItem.setVisible(false);
+			}
+		}
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		
-			case R.id.show_pass: 
+
+		case R.id.show_pass:
+			if (passwordShown) {
 				password.setTransformationMethod(null);
-				break;
-				
-			case R.id.delete_item: 
-				showAlertDialog();
-				break;
-			case R.id.edit_item:
-				editItemListener.loadEditItem( ticket );
-			default: break;
+				passwordShown = false;
+			} else {
+				password.setTransformationMethod(new PasswordTransformationMethod());
+				passwordShown = true;
+			}
+			break;
+
+		case R.id.delete_item:
+			showAlertDialog();
+			break;
+		case R.id.edit_item:
+			editItemListener.loadEditItem(ticket);
+		default:
+			break;
 		}
 		return super.onOptionsItemSelected(item);
-		
+
 	}
-	
+
 	private void showAlertDialog() {
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		builder.setMessage("Are you sure you want to delete the item?")
-		       .setCancelable(false)
-		       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {
-		        	   	dbHelper.deleteRow(ticket.getId());
-						deleteListener.onDeleteItem();
-						showToast("Item has been deleted");
-		           }
-		       })
-		       .setNegativeButton("No", new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {
-		                dialog.cancel();
-		           }
-		       });
-		
+				.setCancelable(false)
+				.setPositiveButton("Yes",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dbHelper.deleteRow(ticket.getId());
+								deleteListener.onDeleteItem();
+								showToast("Item has been deleted");
+							}
+						})
+				.setNegativeButton("No", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+
 		AlertDialog alert = builder.create();
 		builder.show();
 	}
-	
+
 	private void showToast(String string) {
-		Toast t = Toast.makeText(getActivity(), string,
-				Toast.LENGTH_SHORT);
-		t.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER, 0, 0);
+		Toast t = Toast.makeText(getActivity(), string, Toast.LENGTH_SHORT);
+		t.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER, 0, 0);
 		t.show();
 	}
-	
+
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
 		outState.putString("title", title.getText().toString());
@@ -143,12 +160,12 @@ public class ItemsDescription extends Fragment {
 		outState.putString("password", password.getText().toString());
 		outState.putString("notes", notes.getText().toString());
 	}
-	
+
 	private void initUI() {
-		title = (TextView)getView().findViewById(R.id.tv_title);
-		login = (TextView)getView().findViewById(R.id.tv_login_data);
-		password = (TextView)getView().findViewById(R.id.tv_password_data);
-		notes = (TextView)getView().findViewById(R.id.tv_notes_data);
+		title = (TextView) getView().findViewById(R.id.tv_title);
+		login = (TextView) getView().findViewById(R.id.tv_login_data);
+		password = (TextView) getView().findViewById(R.id.tv_password_data);
+		notes = (TextView) getView().findViewById(R.id.tv_notes_data);
 
 		if (ticket != null) {
 			title.setText(ticket.getTitle());
@@ -158,10 +175,10 @@ public class ItemsDescription extends Fragment {
 		}
 	}
 
-    public void onDestroy() {
-    	super.onDestroy();
-    	if (dbHelper != null) {
-    		dbHelper.close();
-    	}
-    }
+	public void onDestroy() {
+		super.onDestroy();
+		if (dbHelper != null) {
+			dbHelper.close();
+		}
+	}
 }
