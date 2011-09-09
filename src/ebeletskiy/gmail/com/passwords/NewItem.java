cb76174a0b35373a78bfdb76401a429ec6ea0a77
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-09-09 16:50:26 +0300

    Issue about possibility to save an item with empty title fixed.

--- src/ebeletskiy/gmail/com/passwords/NewItem.java
+++ src/ebeletskiy/gmail/com/passwords/NewItem.java
@@ -21,117 +21,131 @@ import ebeletskiy.gmail.com.passwords.utils.DBHelper;
 
 public class NewItem extends Fragment {
 	private static final String TAG = "EditItem";
-	
+
 	private boolean menuWasCreated = false;
-	
+
 	DBHelper dbHelper;
 	SaveItemListener saveItemListener;
-	
+
 	EditText title;
 	EditText login;
 	EditText password;
 	EditText notes;
+	Ticket ticket;
 
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
-		saveItemListener = (SaveItemListener)activity;
+		saveItemListener = (SaveItemListener) activity;
 	}
-	
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		if (!menuWasCreated) {
 			Log.i(TAG, "creating menu");
 			setHasOptionsMenu(true);
 			menuWasCreated = true;
 		}
 	}
-	
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.new_item, container, false);
 	}
-	
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
 		dbHelper = new DBHelper(getActivity());
-	}
-	
-	
-	public Ticket createTicket() {
-		Ticket ticket = new Ticket();
-		
-		title = (EditText)getView().findViewById(R.id.et_title);
-		login = (EditText)getView().findViewById(R.id.et_login_data);
-		password = (EditText)getView().findViewById(R.id.et_password_data);
-		notes = (EditText)getView().findViewById(R.id.et_notes_data);
-		
-		ticket.setTitle( (title.getText()).toString() );
-		ticket.setLogin( (login.getText()).toString() );
-		ticket.setPassword( (password.getText()).toString() );
-		ticket.setNotes( (notes.getText()).toString() );
-		
-		return ticket;
+		ticket = new Ticket();
+		initUI();
 	}
 
-	
+	public void initUI() {
+		title = (EditText) getView().findViewById(R.id.et_title);
+		login = (EditText) getView().findViewById(R.id.et_login_data);
+		password = (EditText) getView().findViewById(R.id.et_password_data);
+		notes = (EditText) getView().findViewById(R.id.et_notes_data);
+	}
+
+	public Ticket createTicket() {
+		if (ticket != null) {
+
+			ticket.setTitle((title.getText()).toString());
+			ticket.setLogin((login.getText()).toString());
+			ticket.setPassword((password.getText()).toString());
+			ticket.setNotes((notes.getText()).toString());
+
+			return ticket;
+		} else {
+			throw new NullPointerException("ticket is null");
+		}
+
+	}
+
 	public boolean checkFields() {
 		if (title.getText().toString().equals("")) {
-			return  false;
+			return false;
 		} else {
-			return true; 
+			return true;
 		}
 	}
-	
-	
+
+	public boolean isDuplicate() {
+
+		return false;
+	}
+
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-	    inflater.inflate(R.menu.menu_new_item, menu);
+		inflater.inflate(R.menu.menu_new_item, menu);
 	}
-	
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		
+
 		switch (item.getItemId()) {
-			case R.id.save_item: 
-			
-				dbHelper.insert( createTicket() );
-				
-				if (saveItemListener != null && checkFields()) {
-					InputMethodManager imm =  (InputMethodManager) getActivity().
-						getSystemService(Context.INPUT_METHOD_SERVICE);
-					imm.hideSoftInputFromWindow(title.getWindowToken(), 0);
-					
+		case R.id.save_item:
+
+			if (checkFields()) {
+				if (!isDuplicate()) {
+					createNewItem();
+					hideKeyboard();
 					saveItemListener.saveItem();
-					showToast("New ticket has been created");
 				} else {
-					showToast("Please fill the title");
+					showToast("The item with such name already exists.");
 				}
-				
+			} else {
+				showToast("Please fill the title.");
+			}
 			break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
+	private void hideKeyboard() {
+		InputMethodManager imm = (InputMethodManager) getActivity()
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(title.getWindowToken(), 0);
+	}
+
+	private void createNewItem() {
+		dbHelper.insert(createTicket());
+	}
+
 	private void showToast(String str) {
 		Toast t = Toast.makeText(getActivity(), str, Toast.LENGTH_SHORT);
-		t.setGravity(Gravity.CENTER_HORIZONTAL|Gravity.CENTER, 0, 0);
+		t.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER, 0, 0);
 		t.show();
 	}
-	
-    public void onDestroy() {
-    	super.onDestroy();
-    	if (dbHelper != null) {
-    		dbHelper.close();
-    	}
-    }
+
+	public void onDestroy() {
+		super.onDestroy();
+		if (dbHelper != null) {
+			dbHelper.close();
+		}
+	}
 }
