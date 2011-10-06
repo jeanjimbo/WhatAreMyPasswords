1ca7dafd537b5a0e8c5342c81118048bd4ff86a7
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-10-06 16:37:55 +0300

    About screen added.

--- src/ebeletskiy/gmail/com/passwords/prefs/AboutPreferenceFragment.java
+++ src/ebeletskiy/gmail/com/passwords/prefs/AboutPreferenceFragment.java
@@ -2,13 +2,17 @@ package ebeletskiy.gmail.com.passwords.prefs;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import ebeletskiy.gmail.com.passwords.R;
 
 public class AboutPreferenceFragment extends PreferenceFragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		addPreferencesFromResource(R.xml.about_preferences);
+		addPreferencesFromResource(R.xml.about_preferences);
 	}
 	
 }
