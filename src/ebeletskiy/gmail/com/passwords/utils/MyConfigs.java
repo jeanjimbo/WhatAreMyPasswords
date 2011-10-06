5475090a64de3cee7f0aa7226bf06c67d61e39d0
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-10-06 18:29:00 +0300

    Disable logging.

--- src/ebeletskiy/gmail/com/passwords/utils/MyConfigs.java
+++ src/ebeletskiy/gmail/com/passwords/utils/MyConfigs.java
@@ -4,7 +4,7 @@ public final class MyConfigs {
     private MyConfigs() {
     }
 
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
     public static final int DESTROY_APP_AFTER = 10000; // milliseconds
 
     public static final String PREFS_NAME = "MyPrefsFile";
